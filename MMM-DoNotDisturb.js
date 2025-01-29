0d48c123c361324db460e01689534d10f2020e3f
Travis Lyons <trly@users.noreply.github.com>
2025-01-29 14:15:08 -0500

    refactor status logic to node helper

--- MMM-DoNotDisturb.js
+++ MMM-DoNotDisturb.js
@@ -12,11 +12,11 @@ Module.register('MMM-DoNotDisturb', {
 
   start: function() {
     Log.info('Starting module: ' + this.name)
-    this.eventPool = new Map()
-    this.activeEvent = null
-    Log.debug(`${this.name}: Initializing with check interval ${this.config.checkInterval}ms`)
+    this.activeEvent = false
     this.sendSocketNotification("INIT", {
-      checkInterval: this.config.checkInterval
+      checkInterval: this.config.checkInterval,
+      calendarSet: this.config.calendarSet,
+      includeFullDayEvents: this.config.includeFullDayEvents
     })
   },
 
@@ -27,45 +27,18 @@ Module.register('MMM-DoNotDisturb', {
       if (this.config.calendarSet.length === 0 || 
           this.config.calendarSet.includes(payload.calendarName)) {
         Log.debug(`${this.name}: Adding events from calendar ${payload.calendarName}`)
-        this.eventPool.set(sender.identifier, payload)
-        this.updateCurrentStatus()
-      } else {
-        Log.debug(`${this.name}: Skipping events from calendar ${payload.calendarName} - not in calendarSet`)
+        this.sendSocketNotification("UPDATE_EVENTS", {
+          sender: sender.identifier,
+          events: payload
+        })
       }
     }
   },
 
   socketNotificationReceived: function(notification, payload) {
-    if (notification === "CHECK_EVENTS") {
-      Log.debug(`${this.name}: Received CHECK_EVENTS notification`)
-      this.updateCurrentStatus()
-    }
-  },
-
-  updateCurrentStatus: function() {
-    const now = Date.now()
-    let currentEvents = []
-    
-    Log.debug(`${this.name}: Updating status`)
-    
-    for (const events of this.eventPool.values()) {
-      const activeEvents = events.filter(event => {
-        return (this.config.calendarSet.length === 0 || 
-                this.config.calendarSet.includes(event.calendarName)) &&
-               ((event.startDate <= now && event.endDate >= now) ||
-                (this.config.includeFullDayEvents && event.fullDayEvent))
-      })
-      currentEvents = currentEvents.concat(activeEvents)
-    }
-    
-    const wasActive = this.activeEvent
-    this.activeEvent = currentEvents.length > 0
-    
-    if (wasActive !== this.activeEvent) {
-      Log.info(`${this.name}: DND Status changed to: ${this.activeEvent ? 'Active' : 'Inactive'}`)
-      if (this.activeEvent) {
-        Log.debug(`${this.name}: Found ${currentEvents.length} active events`)
-      }
+    if (notification === "STATUS_CHANGED") {
+      Log.debug(`${this.name}: Received status change: active=${payload.active}`)
+      this.activeEvent = payload.active
       this.updateDom(this.config.animationSpeed)
     }
   },
