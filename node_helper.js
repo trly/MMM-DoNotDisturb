fb2abe113498e0e7f8dcfe857a92ec9d7ce4bbc4
Travis Lyons <trly@users.noreply.github.com>
2025-02-01 01:17:55 -0500

    Revert "refactor status logic to node helper"
    This reverts commit 0d48c123c361324db460e01689534d10f2020e3f.

--- node_helper.js
+++ node_helper.js
@@ -3,61 +3,27 @@ const NodeHelper = require("node_helper")
 module.exports = NodeHelper.create({
   start: function() {
     this.timer = null
-    this.eventPool = new Map()
-    this.activeEvent = false
-    Log.log(`Starting node helper for: ${this.name}`);
+    console.log("Starting node_helper for: " + this.name)
   },
 
   socketNotificationReceived: function(notification, payload) {
-    switch(notification) {
-      case "INIT":
-        Log.debug(`${this.name}: Received INIT notification with interval ${payload.checkInterval}ms`)
-        this.config = payload
-        this.startMonitoring(payload.checkInterval)
-        break
-      case "UPDATE_EVENTS":
-        this.eventPool.set(payload.sender, payload.events)
-        this.updateCurrentStatus()
-        break
+    if (notification === "INIT") {
+      console.log(`${this.name}: Received INIT notification with interval ${payload.checkInterval}ms`)
+      this.startMonitoring(payload.checkInterval)
     }
   },
 
   startMonitoring: function(interval) {
-    Log.log(`${this.name}: Starting event monitoring every ${interval}ms`)
+    console.log(`${this.name}: Starting event monitoring with ${interval}ms interval`)
     this.timer = setInterval(() => {
-      this.updateCurrentStatus()
+      console.log(`${this.name}: Checking events...`)
+      this.sendSocketNotification("CHECK_EVENTS")
     }, interval)
   },
 
-  updateCurrentStatus: function() {
-    const now = Date.now()
-    let currentEvents = []
-    
-    for (const events of this.eventPool.values()) {
-      const activeEvents = events.filter(event => {
-        return (this.config.calendarSet.length === 0 || 
-                this.config.calendarSet.includes(event.calendarName)) &&
-               ((event.startDate <= now && event.endDate >= now) ||
-                (this.config.includeFullDayEvents && event.fullDayEvent))
-      })
-      currentEvents = currentEvents.concat(activeEvents)
-    }
-    
-    const wasActive = this.activeEvent
-    this.activeEvent = currentEvents.length > 0
-    
-    if (wasActive !== this.activeEvent) {
-      Log.log(`${this.name}: DND Status changed to: ${this.activeEvent ? 'Active' : 'Inactive'}`)
-      this.sendSocketNotification("STATUS_CHANGED", {
-        active: this.activeEvent,
-        eventCount: currentEvents.length
-      })
-    }
-  },
-
   stop: function() {
-    Log.log(`Stopping node helper for: ${this.name}`);
     if (this.timer) {
+      console.log("Stopping node_helper for: " + this.name)
       clearInterval(this.timer)
     }
   }
